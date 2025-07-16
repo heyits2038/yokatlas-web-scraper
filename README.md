c73e89a1d87f1fd1d30739bb9fbfa9e0acc3fdf9
heyits2038 <220956554+heyits2038@users.noreply.github.com>
2025-07-17 00:02:43 +0300

    chore: `init`

---
+++ README.md
@@ -0,0 +1,1 @@
+# YOKAtlas Web Scraper
---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+    Author: @heyits2038
+    GitHub: https://github.com/heyits2038
+"""
+
+if __name__ == "__main__":
+    print("[DEBUG]: Let's start developing!")
