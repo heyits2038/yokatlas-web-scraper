894c7ffc2c62e898e549c4015a70561b6c642cb0
heyits2038 <220956554+heyits2038@users.noreply.github.com>
2025-07-20 15:35:30 +0300

    chore: add function to transform `turkish` keys to `english`

--- yokatlas/utils.py
+++ yokatlas/utils.py
@@ -2,6 +2,7 @@
     Author: @heyits2038
     GitHub: https://github.com/heyits2038
 """
+import re
 from typing import Optional
 
 from bs4 import BeautifulSoup
@@ -25,3 +26,15 @@ def get_html_content(url: str) -> Optional[BeautifulSoup]:
         return BeautifulSoup(driver.page_source, "html.parser")
     finally:
         driver.quit()
+
+
+def transform_key_to_english(key: str):
+    """
+    Transforms a Turkish key to its English key equivalent.
+    """
+    turkish_chars = "şŞıİçÇöÖüÜğĞ"
+    english_chars = "sSiIcCoOuUgG"
+    table = str.maketrans(turkish_chars, english_chars)
+    key = key.translate(table).lower()
+
+    return re.sub(r"\W+", "", key)
