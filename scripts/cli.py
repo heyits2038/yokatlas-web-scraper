c587c782057f522ad98ada9cd11026814f318008
heyits2038 <220956554+heyits2038@users.noreply.github.com>
2025-07-21 17:07:36 +0300

    refactor: split main into `print_welcome` and `bachelor_degree_programs` functions

--- scripts/cli.py
+++ scripts/cli.py
@@ -9,19 +9,31 @@ import pyfiglet
 
 CHOICES = {
     "bachelor": "📘 Bachelor's Degree Programs",
-    "associate": "📗 Associate Degree Programs",
     "exit": "❌ Exit"
 }
+
+
+def bachelor_degree_programs() -> None:
+    """
+    Fetches and displays information about bachelor's degree programs.
+    """
+    pass
+
+
+def print_welcome() -> None:
+    """
+    Prints a welcome message.
+    """
+    print(pyfiglet.figlet_format("YokAtlas", font="slant"))
+    print(pyfiglet.figlet_format("Web Scraper", font="slant"))
+    print("⚠️  This program is for educational purposes only.\n")
 
 
 def main() -> None:
     """"
     Displays a welcome message and prompts the user to select a degree program to fetch.
     """
-    print(pyfiglet.figlet_format("YokAtlas", font="slant"))
-    print(pyfiglet.figlet_format("Web Scraper", font="slant"))
-
-    print("⚠️  This program is for educational purposes only.\n")
+    print_welcome()
 
     choice = questionary.select(
         "🎓 Which type of degree program would you like to fetch?",
@@ -30,9 +42,7 @@ def main() -> None:
 
     try:
         if choice == CHOICES["bachelor"]:
-            pass
-        elif choice == CHOICES["associate"]:
-            pass
+            bachelor_degree_programs()
         elif choice == CHOICES["exit"]:
             sys.exit(0)
         else:
