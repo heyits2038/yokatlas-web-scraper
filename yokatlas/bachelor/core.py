928d605c425447d907b94734ad2e45acbd21e651
heyits2038 <220956554+heyits2038@users.noreply.github.com>
2025-07-21 17:09:18 +0300

    feat: add `get_program_general_info_by_id` function

--- yokatlas/bachelor/core.py
+++ yokatlas/bachelor/core.py
@@ -17,3 +17,10 @@ def get_programs_by_university_id(university_id: str):
     Get the list of programs for a given university ID.
     """
     return scraper.fetch_programs_by_university_id(university_id)
+
+
+def get_program_general_info_by_id(program_id: str):
+    """
+    Get the general information for a given program ID.
+    """
+    return scraper.fetch_program_general_info_by_id(program_id)
