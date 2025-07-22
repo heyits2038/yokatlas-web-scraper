f1865b016774e9efc6ad6ae505ad2d1c6f4a9105
heyits2038 <220956554+heyits2038@users.noreply.github.com>
2025-07-22 14:04:44 +0300

    feat: add `json operations` and export `utils` module (#16)

--- yokatlas/__init__.py
+++ yokatlas/__init__.py
@@ -3,5 +3,6 @@ Author: @heyits2038
 GitHub: https://github.com/heyits2038
 """
 from .bachelor import *
+from .utils import *
 
-__all__ = ["bachelor"]
+__all__ = ["bachelor", "utils"]
--- yokatlas/utils.py
+++ yokatlas/utils.py
@@ -2,13 +2,16 @@
     Author: @heyits2038
     GitHub: https://github.com/heyits2038
 """
+import os
 import re
+import json
 from typing import Optional
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from loguru import logger
 
 
 def get_html_content(url: str) -> Optional[BeautifulSoup]:
@@ -38,3 +41,35 @@ def transform_key_to_english(key: str):
     key = key.translate(table).lower()
 
     return re.sub(r"\W+", "", key)
+
+
+def save_json_file(data, filename):
+    """
+    Saves the given data to a JSON file.
+    """
+    try:
+        directory = os.path.dirname(filename)
+
+        if directory:
+            os.makedirs(directory, exist_ok=True)
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+        return True
+    except Exception as e:
+        logger.error(f"Error saving JSON file: {e}")
+        return False
+
+
+def read_json_file(filename):
+    """
+    Reads a JSON file.
+    """
+    try:
+        if not os.path.exists(filename):
+            logger.error(f"File not found: {filename}")
+            return None
+        with open(filename, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except Exception as e:
+        logger.error(f"Error reading JSON file: {e}")
+        return None
